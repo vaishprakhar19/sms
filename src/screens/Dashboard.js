17aff18ef0bfc8719d56e6784c51085b6e0b4aa6
Prakhar Vaish <126570561+vaishprakhar19@users.noreply.github.com>
2024-04-23 19:26:53 +0530

    progress in dash

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import './dashboard.css';
 
 function Dashboard() {
   return (
-    <div>dashboard</div>
+    <div className='dashboard'>
+      <header>
+        <div className='user-profile'>
+          <img className='user-profile-image' src='/' alt='profile-image' />
+          <p>Username</p>
+        </div>
+      </header>
+      <main className='dash-item-container'>
+        <div className='dash-item'>
+          <img src='/' alt='dash-item-image' />
+          <p>Dashboard Item</p>
+        </div>
+        <div className='dash-item'>
+          <img src='/' alt='dash-item-image' />
+          <p>Dashboard Item</p>
+        </div>
+        <div className='dash-item'>
+          <img src='/' alt='dash-item-image' />
+          <p>Dashboard Item</p>
+        </div>
+        <div className='dash-item'>
+          <img src='/' alt='dash-item-image' />
+          <p>Dashboard Item</p>
+        </div>
+      </main>
+    </div>
   )
 }
 
