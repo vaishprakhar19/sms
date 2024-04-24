6fc945d6383c05835042366c689f4ce0b0d0b95a
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2024-04-24 23:35:19 +0530

    backend import

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes, BrowserRouter as Router, Navigate } from 'react-router-d
 import './App.css';
 import Dashboard from './screens/Dashboard';
 import Login from './screens/Login';
-<<<<<<< HEAD:src/App.js
+
 import Register from "./screens/Register"
 import React,{useState} from 'react';
 import Result from './screens/Result';
@@ -12,10 +12,7 @@ import MessTiming from './screens/MessTiming';
 import Syllabus from './screens/Syllabus';
 import Holidays from './screens/Holidays';
 import PYQ from './screens/PYQ';
-=======
-import React,{useState} from 'react'
-import MessTiming from './screens/MessTiming';
->>>>>>> 8872c4f8174782e0a1eedd998b001117bee31470:client/src/App.js
+
 
 function App() {
   const [user, setUser] = useState(null);
@@ -29,7 +26,7 @@ function App() {
           <>
          
           <Route path="/dashboard" element={<Dashboard user={user} setUser={setUser}/>}></Route>
-<<<<<<< HEAD:src/App.js
+
           <Route path="/result" element={<Result/>}></Route>
           <Route path="/timetable" element={<TimeTable/>}></Route>
           <Route path="/messmenu" element={<MessMenu/>}></Route>
@@ -37,9 +34,8 @@ function App() {
           <Route path="/syllabus" element={<Syllabus/>}></Route>
           <Route path="/holidays" element={<Holidays/>}></Route>
           <Route path="/pyq" element={<PYQ/>}></Route>
-=======
           <Route path="/mess_timing" element={<MessTiming />} ></Route>
->>>>>>> 8872c4f8174782e0a1eedd998b001117bee31470:client/src/App.js
+
           <Route path="*" element={<Navigate to="/dashboard"/>}></Route>
           
           </>
