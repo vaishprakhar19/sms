4a2eb76b9aa2ba07b073b0ad47818ab7cbf64d7d
Prakhar Vaish <126570561+vaishprakhar19@users.noreply.github.com>
2024-04-25 01:31:11 +0530

    firefuck

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Dashboard from './screens/Dashboard';
 import Login from './screens/Login';
 
 import Register from "./screens/Register"
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Result from './screens/Result';
 import TimeTable from './screens/TimeTable';
 import MessMenu from './screens/MessMenu';
@@ -13,51 +13,60 @@ import Syllabus from './screens/Syllabus';
 import Holidays from './screens/Holidays';
 import PYQ from './screens/PYQ';
 import { doc, getDoc } from "firebase/firestore";
-import { db } from './firebase';
+import { auth, db } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
 
 function App() {
   const [user, setUser] = useState(null);
   const [isRegistered, setIsRegistered] = useState(false);
   // console.log(user);
-  console.log(db.collection('user').doc(user.uid).get());
+  // console.log(db.collection('user').doc(user.uid).get());
 
-useEffect(async ()=>{
-  const docRef = doc(db, "user",user.uid);
-  const docSnap = await getDoc(docRef);
-  console.log(docSnap.data());
-})
-  
-  
+// onAuthStateChanged(auth,(user)=>{
+//   if(user){
+//     setUser(user);
+//   } else{
+//     setUser(null);
+//   }
+// })
+
+//     const getUser = async ()=>{
+//       const docRef = doc(db, "user",user.uid);
+//       const docSnap = await getDoc(docRef);
+//       console.log(docSnap.data());
+//     }
+//   useEffect(()=>{
+//     getUser();
+//   },[user])
+
+
 
   return (
     <div className="App">
       <Router>
         <Routes>
-        <Route path="/register" element={<Register isRegistered={isRegistered} setIsRegistered={setIsRegistered} />}></Route>
-          {user?
-          <>
-          <Route path="/dashboard" element={<Dashboard user={user} setUser={setUser}/>}></Route>
-
-          <Route path="/result" element={<Result/>}></Route>
-          <Route path="/timetable" element={<TimeTable/>}></Route>
-          <Route path="/messmenu" element={<MessMenu/>}></Route>
-          <Route path="/messtiming" element={<MessTiming/>}></Route>
-          <Route path="/syllabus" element={<Syllabus/>}></Route>
-          <Route path="/holidays" element={<Holidays/>}></Route>
-          <Route path="/pyq" element={<PYQ/>}></Route>
-          <Route path="/mess_timing" element={<MessTiming />} ></Route>
-
-          <Route path="*" element={<Navigate to="/dashboard"/>}></Route>
-          
-          </>
-          :
-          <>
-          <Route path="/login" element={<Login user={user} setUser={setUser}/>}></Route>
-          <Route path="*" element={<Navigate to="/login"/>}></Route>
-          </>
-}
-        </Routes> 
+          <Route path="/register" element={<Register user={user} setIsRegistered={setIsRegistered} />}></Route>
+          {user && isRegistered ?
+            <>
+              <Route path="/dashboard" element={<Dashboard user={user} setUser={setUser} />}></Route>
+              <Route path="/result" element={<Result />}></Route>
+              <Route path="/timetable" element={<TimeTable />}></Route>
+              <Route path="/messmenu" element={<MessMenu />}></Route>
+              <Route path="/messtiming" element={<MessTiming />}></Route>
+              <Route path="/syllabus" element={<Syllabus />}></Route>
+              <Route path="/holidays" element={<Holidays />}></Route>
+              <Route path="/pyq" element={<PYQ />}></Route>
+              <Route path="/mess_timing" element={<MessTiming />} ></Route>
+              <Route path="*" element={<Navigate to="/dashboard" />}></Route>
+            </>
+            :
+            <>
+              <Route path="/login" element={<Login user={user} setUser={setUser} isRegistered={isRegistered}/>}></Route>
+              <Route path="*" element={<Navigate to="/login" />}></Route>
+            </>
+          }
+        </Routes>
       </Router>
     </div>
   );
--- client/src/screens/Register.js
+++ client/src/screens/Register.js
@@ -1,81 +1,99 @@
 
 import React from 'react'
 import "./Register.css"
+import { setDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase'
+import { useNavigate } from 'react-router-dom'
 
-const Register = () => {
+
+const Register = ({ user, setIsRegistered }) => {
+
+  const navigate = useNavigate();
+
+  const setReg = async () => {
+    await setDoc(doc(db, "user", user.uid), {
+      isRegistered: true
+    });
+  }
+  const handleRegister = () => {
+    setReg();
+    setIsRegistered(true);
+    navigate('/dashboard');
+  }
+
   return (
     <div className='register'>
-        <div className="container">
-  <div className="heading">SignIn to your account</div>
-  <form className="form" action="">
-    <div className="input-field">
-      <input
-        required="on"
-        autocomplete="off"
-        type="text"
-        name="text"
-        id="username"
-        placeholder=''
-      />
-      <label for="username">Name</label>
-    </div>
-    <div className="input-field">
-      <input
-        required="on"
-        autocomplete="off"
-        type="text"
-        inputMode='numeric'
-        pattern="[0-9]*"
-        name="number"
-        id="email"
-        placeholder=''
-      />
-      <label for="email">Mobile No.</label>
-    </div>
-    <div className="input-field">
-      <input
-        required="on"
-        type="text"
-        inputMode='numeric'
-        pattern="[0-9]*"
-        name="number"
-        id="password"
-        placeholder=''
-      />
-      <label for="username">Roll No.</label>
-    </div>
-    <div className="input-field">
-      <input
-        required="on"
-        autocomplete="off"
-        type="password"
-        name="text"
-        id="password"
-        placeholder=''
-      />
-      <label for="username">Password</label>
-    </div>
-    <div className="radio-inputs">
-  <label className="radio">
-    <input type="radio" name="radio" />
-    <span className="name">CSE</span>
-  </label>
-  <label className="radio">
-    <input type="radio" name="radio"/>
-    <span className="name">ECE</span>
-  </label>
-      
-  <label className="radio">
-    <input type="radio" name="radio"/>
-    <span className="name">MCA</span>
-  </label>
-</div>
+      <div className="container">
+        <div className="heading">SignIn to your account</div>
+        <form className="form" onSubmit={handleRegister}>
+          <div className="input-field">
+            <input
+              required="on"
+              autocomplete="off"
+              type="text"
+              name="text"
+              id="username"
+              placeholder=''
+            />
+            <label for="username">Name</label>
+          </div>
+          <div className="input-field">
+            <input
+              required="on"
+              autocomplete="off"
+              type="text"
+              inputMode='numeric'
+              pattern="[0-9]*"
+              name="number"
+              id="email"
+              placeholder=''
+            />
+            <label for="email">Mobile No.</label>
+          </div>
+          <div className="input-field">
+            <input
+              required="on"
+              type="text"
+              inputMode='numeric'
+              pattern="[0-9]*"
+              name="number"
+              id="password"
+              placeholder=''
+            />
+            <label for="username">Roll No.</label>
+          </div>
+          <div className="input-field">
+            <input
+              required="on"
+              autocomplete="off"
+              type="password"
+              name="text"
+              id="password"
+              placeholder=''
+            />
+            <label for="username">Password</label>
+          </div>
+          <div className="radio-inputs">
+            <label className="radio">
+              <input type="radio" name="radio" />
+              <span className="name">CSE</span>
+            </label>
+            <label className="radio">
+              <input type="radio" name="radio" />
+              <span className="name">ECE</span>
+            </label>
 
-    <div className="btn-container">
-      <button className="btn">Submit</button>
-    </div>
-  </form>
-</div>
+            <label className="radio">
+              <input type="radio" name="radio" />
+              <span className="name">MCA</span>
+            </label>
+          </div>
+
+          <div className="btn-container">
+            <button className="btn">Submit</button>
+          </div>
+        </form>
+      </div>
 
     </div>
   )
