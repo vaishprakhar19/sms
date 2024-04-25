9da869153de8935eaa1b38e8a120e6e68b0eac4c
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2024-04-25 15:53:42 +0530

    PNGs

--- client/src/components/dashdata.js
+++ client/src/components/dashdata.js
@@ -1,33 +1,38 @@
 export default [
     {
-        img:"https://cdn-icons-png.flaticon.com/512/9913/9913579.png",
+        img:"https://firebasestorage.googleapis.com/v0/b/student-portal-46087.appspot.com/o/lunch-time.png?alt=media&token=ffb2a13c-94cd-42a9-aace-85467c7e6da3",
         title: "Mess timing",
         href: "/mess_timing"
     },
     {
         link:"/messmenu",
-        img:"https://cdn-icons-png.flaticon.com/512/9913/9913579.png",
+        img:"https://firebasestorage.googleapis.com/v0/b/student-portal-46087.appspot.com/o/diet.png?alt=media&token=cbb28b5b-b617-4f04-a879-403ecbdd4b19",
         title: "Mess Menu"
     },
     {
         link:"/timetable",
-        img:"https://cdn-icons-png.flaticon.com/512/9913/9913579.png",
+        img:"https://firebasestorage.googleapis.com/v0/b/student-portal-46087.appspot.com/o/table.png?alt=media&token=e7e54522-30d6-4e78-ae0b-b31dcaa3197c",
         title: "Time Table"
     },
     {
         link:"/holidays",
-        img:"https://cdn-icons-png.flaticon.com/512/9913/9913579.png",
+        img:"https://firebasestorage.googleapis.com/v0/b/student-portal-46087.appspot.com/o/schedule.png?alt=media&token=6f4cd235-df31-475c-b69a-56cdb7c59350",
         title: "Holidays"
     },
     {
         link:"/pyq",
-        img:"https://cdn-icons-png.flaticon.com/512/9913/9913579.png",
+        img:"https://firebasestorage.googleapis.com/v0/b/student-portal-46087.appspot.com/o/exam.png?alt=media&token=39a820fe-9221-48e8-a4a0-e4500c21f562",
         title: "PYQ"
     },
     {
         link:"/syllabus",
-        img:"https://cdn-icons-png.flaticon.com/512/9913/9913579.png",
+        img:"https://firebasestorage.googleapis.com/v0/b/student-portal-46087.appspot.com/o/checklist.png?alt=media&token=c2dabb28-c8f1-4ae8-99ca-d5dcacc108f8",
         title: "Syllabus"
+    },
+    {
+        link:"/",
+        img:"https://firebasestorage.googleapis.com/v0/b/student-portal-46087.appspot.com/o/score.png?alt=media&token=ac32c60c-db26-420b-a58e-6a373926983c",
+        title: "Result"
     }
 
 ]
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import {getFirestore} from "firebase/firestore"
 
@@ -15,7 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 const auth = getAuth();
