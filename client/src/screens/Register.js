6ed58c8717ad6654c7e579a78a9553c4c42081f7
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2024-04-25 12:33:05 +0530

    redirect fixed

--- client/src/screens/Register.js
+++ client/src/screens/Register.js
@@ -1,109 +1,100 @@
-
-import React from 'react'
-import "./Register.css"
-import { setDoc, doc } from 'firebase/firestore'
-import { db } from '../firebase'
-import { useNavigate } from 'react-router-dom'
-
+import React from "react";
+import "./Register.css";
+import { setDoc, doc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Register = ({ user, setIsRegistered }) => {
-
   const navigate = useNavigate();
-  
+
   const setReg = async () => {
-if(user){
-    await setDoc(doc(db, "user", user.uid), {
-      isRegistered: true
-    });
-  }
-}
+    if (user) {
+      await setDoc(doc(db, "user", user.uid), {
+        isRegistered: true,
+      });
+    }
+  };
   const handleRegister = () => {
     setReg();
     setIsRegistered(true);
-    navigate('/dashboard');
-  }
+    navigate("/dashboard");
+  };
 
   return (
-    <div className='register'>
-        <div className="container">
-  <div className="heading">Enter Your Information</div>
-  <form className="form" action="">
-    <div className="input-field">
-      <input
-        required="on"
-        autocomplete="off"
-        type="text"
-        name="text"
-        id="username"
-        placeholder=''
-      />
-      <label for="username">Name</label>
-    </div>
-    <div className="input-field">
-      <input
-        required="on"
-        autocomplete="off"
-        type="text"
-        inputMode='numeric'
-        pattern="[^A-Za-z]*"
-        name="number"
-        id="email"
-        placeholder=''
-      />
-      <label for="email">Mobile No.</label>
-    </div>
-    <div className="input-field">
-      <input
-        required="on"
-        type="text"
-        inputMode='numeric'
-        pattern="[0-9]*"
-        name="number"
-        id="password"
-        placeholder=''
-      />
-      <label for="username">Roll No.</label>
-    </div>
-    <div className="input-field">
-      <input
-        required="on"
-        autocomplete="off"
-        type="text"
-        inputMode='numeric'
-        pattern="[0-9]*"
-        name="text"
-        id="password"
-        placeholder=''
-      />
-      <label for="username">Batch</label>
-    </div>
-  
-    <div className='radio-inputs'> 
-  
-    <label className="radio male">
-    <input type="radio" name="radio" />
-    <span className="name">Male</span>
-  </label>
-  <label className="radio female">
-    <input type="radio" name="radio"/>
-    <span className="name">Female</span>
-  </label>
-    </div>
-    <div className="radio-inputs">
-  <label className="radio">
-    <input type="radio" name="radio" />
-    <span className="name">CSE</span>
-  </label>
-  <label className="radio">
-    <input type="radio" name="radio"/>
-    <span className="name">ECE</span>
-  </label>
-      
-  <label className="radio">
-    <input type="radio" name="radio"/>
-    <span className="name">MCA</span>
-  </label>
-</div>
+    <div className="register">
+      <div className="container">
+        <div className="heading">Enter Your Information</div>
+        <form className="form" onSubmit={handleRegister}>
+          <div className="input-field">
+            <input
+              required="on"
+              autocomplete="off"
+              type="text"
+              name="text"
+              id="username"
+              placeholder=""
+            />
+            <label for="username">Name</label>
+          </div>
+          <div className="input-field">
+            <input
+              required="on"
+              autocomplete="off"
+              type="text"
+              inputMode="numeric"
+              pattern="[^A-Za-z]*"
+              name="number"
+              id="email"
+              placeholder=""
+            />
+            <label for="email">Mobile No.</label>
+          </div>
+          <div className="input-field">
+            <input
+              required="on"
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9]*"
+              name="number"
+              id="password"
+              placeholder=""
+            />
+            <label for="username">Roll No.</label>
+          </div>
+          <div className="input-field">
+            <input
+              required="on"
+              autocomplete="off"
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9]*"
+              name="text"
+              id="password"
+              placeholder=""
+            />
+            <label for="username">Batch</label>
+          </div>
+
+          <div className="radio-inputs">
+            <label className="radio male">
+              <input type="radio" name="radio" />
+              <span className="name">Male</span>
+            </label>
+            <label className="radio female">
+              <input type="radio" name="radio" />
+              <span className="name">Female</span>
+            </label>
+          </div>
+          <div className="radio-inputs">
+            <label className="radio">
+              <input type="radio" name="radio" />
+              <span className="name">CSE</span>
+            </label>
+            <label className="radio">
+              <input type="radio" name="radio" />
+              <span className="name">ECE</span>
+            </label>
+
             <label className="radio">
               <input type="radio" name="radio" />
               <span className="name">MCA</span>
@@ -115,9 +106,8 @@ if(user){
           </div>
         </form>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
