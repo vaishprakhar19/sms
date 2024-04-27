96b48bf4ecc442fe32caf67f9222ca710d40abf9
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2024-04-27 23:08:03 +0530

    nav bar responsive

--- client/src/screens/Loader.js
+++ client/src/screens/Loader.js
@@ -1,42 +1,14 @@
 import React from 'react'
-import Card from '../components/Card';
-import dashdata from '../components/dashdata';
-import Notices from '../components/Notices';
-import notidata from '../components/notidata';
-import './loader.css';
+import "./loader.css"
 
-
-const Loader = () => {
-    const cards = dashdata.map(item => {
-        return (
-            <Card/>
-        )
-    })
-
-    const notices = notidata.map(item => {
-        return (
-            <Notices/>
-        )
-    })
-    return (
-        <div className='dashboard'>
-            <header>
-                <div className='Loader-user-profile'>
-                    <img className='Loader-user-profile-image' src="" alt='profile' />
-                    <p>User</p>
-                    <button className="Loader-Logout-Btn">H</button>
-                </div>
-            </header>
-            <main>
-                <card className='Loader-dash-item-container'>
-                    {cards}
-                </card>
-            </main>
-            <notice>
-                {notices}
-            </notice>
-        </div>
-    )
+const Loader = ({loading}) => {
+  return (<>
+    {loading?
+    <div className='loader'>Loading.......</div>
+    :<></>
+    }
+  </>
+  )
 }
 
 export default Loader
