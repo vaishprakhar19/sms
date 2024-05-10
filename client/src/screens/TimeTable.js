df68c103e249642e323427c103e25819b3641b9f
Prakhar Vaish <126570561+vaishprakhar19@users.noreply.github.com>
2024-05-10 16:14:12 +0530

    css update

--- client/src/screens/TimeTable.js
+++ client/src/screens/TimeTable.js
@@ -3,69 +3,74 @@ import "./Timetable.css"
 const TimeTable = () => {
   return (
     <div>
-      <table   className='table'> 
-    <tr> 
-    <th colspan ="8">timetable</th>  
-    </tr> 
-    <tr> 
-    <th rowspan ="8">hours</th> 
-    <th> mon</th> 
-    <th> tue</th> 
-    <th> wed</th> 
-    <th> thurs</th> 
-    <th> fri</th> 
-    <th> Sat</th> 
-    </tr> 
-    <tr> 
-    <td>science</td> 
-    <td>maths</td>
-    <td>science</td>
-    <td>maths</td> 
-    <td>arts</td> 
-    <td>arts</td> 
-    </tr> 
-    <tr> 
-    <td>science</td> 
-    <td>maths</td>
-    <td>science</td>
-    <td>maths</td> 
-    <td>arts</td> 
-    <td>arts</td> 
-    </tr> 
-    <tr> 
-    <td>science</td> 
-    <td>maths</td>
-    <td>science</td>
-    <td>maths</td> 
-    <td>arts</td> 
-    <td>arts</td> 
-    </tr> 
-    <tr> 
-    <td>social</td> 
-    <td>hindi</td>
-    <td>english</td>
-    <td>social</td> 
-    <td>sports</td> 
-    <td>sports</td> 
-    </tr> 
-    <tr> 
-    <th colspan="6">lunch</th> 
-    </tr> 
-    <tr> 
-    <td>science</td> 
-    <td>maths</td>
-    <td>science</td>
-    <td>maths</td> 
-    <td rowspan="1">project</td>
-    </tr> 
-    <tr> 
-    <td>social</td> 
-    <td>hindi</td>
-    <td>english</td>
-    <td>social</td> 
-    <td>social</td> 
-    </tr> 
-    </table>
+      <div className='page-header'>
+        <h2>Time Table</h2>
+      </div>
+      <div className='page-layout'>
+        <table className='table'>
+          <tr>
+            <th colspan="8">timetable</th>
+          </tr>
+          <tr>
+            <th rowspan="8">hours</th>
+            <th> mon</th>
+            <th> tue</th>
+            <th> wed</th>
+            <th> thurs</th>
+            <th> fri</th>
+            <th> Sat</th>
+          </tr>
+          <tr>
+            <td>science</td>
+            <td>maths</td>
+            <td>science</td>
+            <td>maths</td>
+            <td>arts</td>
+            <td>arts</td>
+          </tr>
+          <tr>
+            <td>science</td>
+            <td>maths</td>
+            <td>science</td>
+            <td>maths</td>
+            <td>arts</td>
+            <td>arts</td>
+          </tr>
+          <tr>
+            <td>science</td>
+            <td>maths</td>
+            <td>science</td>
+            <td>maths</td>
+            <td>arts</td>
+            <td>arts</td>
+          </tr>
+          <tr>
+            <td>social</td>
+            <td>hindi</td>
+            <td>english</td>
+            <td>social</td>
+            <td>sports</td>
+            <td>sports</td>
+          </tr>
+          <tr>
+            <th colspan="6">lunch</th>
+          </tr>
+          <tr>
+            <td>science</td>
+            <td>maths</td>
+            <td>science</td>
+            <td>maths</td>
+            <td rowspan="1">project</td>
+          </tr>
+          <tr>
+            <td>social</td>
+            <td>hindi</td>
+            <td>english</td>
+            <td>social</td>
+            <td>social</td>
+          </tr>
+        </table>
+      </div>
     </div>
   )
 }
