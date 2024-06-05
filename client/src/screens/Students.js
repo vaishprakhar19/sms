350c4b87af0811c53fd74d3ac2df3d742a35b7b2
Prakhar Vaish <126570561+vaishprakhar19@users.noreply.github.com>
2024-06-05 15:26:19 +0530

    dsdfs

--- client/src/screens/Students.js
+++ client/src/screens/Students.js
@@ -5,54 +5,53 @@ import * as XLSX from "xlsx";
 import "./Students.css"
 
 const Students = () => {
-    const [users, setUsers] = useState([]);
-    const [filters, setFilters] = useState({
-      semester: '',
-      department: '',
-      gender: ''
-    });
-  
-    const fetchUsers = () => {
-      const params = {
-        semester: filters.semester,
-        department: filters.department,
-        gender: filters.gender
-      };
-  
-      axios.get("/api/users", { params })
-        .then(response => {
-          setUsers(response.data);
-        })
-        .catch(error => {
-          console.error("Error fetching user data:", error);
-        });
+  const [users, setUsers] = useState([]);
+  const [filters, setFilters] = useState({
+    semester: '',
+    department: '',
+    gender: ''
+  });
+
+  const fetchUsers = () => {
+    const params = {
+      semester: filters.semester,
+      department: filters.department,
+      gender: filters.gender
     };
-  
-    useEffect(() => {
-      fetchUsers();
-    }, [filters]);
-  
-    const handleFilterChange = (e) => {
-      const { name, value } = e.target;
-      setFilters({ ...filters, [name]: value });
-    };
-  
-    const exportToExcel = () => {
-      const worksheet = XLSX.utils.json_to_sheet(users);
-      const workbook = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(workbook, worksheet, "Users");
-      XLSX.writeFile(workbook, "users.xlsx");
-    };
-  
-    return (
-      <div>
-        <div className="page-header">
+
+    axios.get("/api/users", { params })
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching user data:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [filters]);
+
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters({ ...filters, [name]: value });
+  };
+
+  const exportToExcel = () => {
+    const worksheet = XLSX.utils.json_to_sheet(users);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Users");
+    XLSX.writeFile(workbook, "users.xlsx");
+  };
+
+  return (
+    <div>
+      <div className="page-header">
 
         <h2>User Data</h2>
-        </div>
-        <div className="page-layout">
-            <div className="form_main notice-form">
-
+      </div>
+      <div className="page-layout">
+        <div className="form_main notice-form">
           <label>
             Batch:
             <input type="text" className="inputField" name="semester" value={filters.semester} onChange={handleFilterChange} />
@@ -66,12 +65,12 @@ const Students = () => {
             <input type="text" className="inputField" name="gender" value={filters.gender} onChange={handleFilterChange} />
           </label>
           <button id="button" onClick={fetchUsers}>Filter</button>
-            </div>
         </div>
+        <div className="table-container">
+
         <table className="table">
           <thead>
             <tr>
-              
               <th>Name</th>
               <th>Mobile</th>
               <th>Roll No</th>
@@ -83,7 +82,7 @@ const Students = () => {
           <tbody>
             {users.map(user => (
               <tr key={user.uid}>
-            
+
                 <td>{user.name}</td>
                 <td>{user.mobile}</td>
                 <td>{user.rollNo}</td>
@@ -94,9 +93,11 @@ const Students = () => {
             ))}
           </tbody>
         </table>
+        </div>
         <button id="button" onClick={exportToExcel}>Export to Excel</button>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Students;
