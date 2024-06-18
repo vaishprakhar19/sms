8ca269e9c45c5ae5cf0f895e41a613f55f5c6a58
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2024-06-19 01:04:29 +0530

    syllabus changed

---
+++ client/src/components/Syllabusembed.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./PYQEmbed.css"
+const Syllabusembed = ({ fileId }) => {
+  const embedUrl = `https://drive.google.com/file/d/${fileId}/preview`;
+
+  return (
+    <div className="drive-embed-container">
+    <iframe
+      src={embedUrl}
+      title="Google Drive"
+      width="100%"
+      height="100%"
+      allow="autoplay"
+    ></iframe>
+  </div>
+  )
+};
+
+export default Syllabusembed
