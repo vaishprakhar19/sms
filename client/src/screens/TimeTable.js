86bae6efeb8ddd348ef0a8808ceb7fb1cbd89810
Prakhar Vaish <126570561+vaishprakhar19@users.noreply.github.com>
2024-06-20 13:26:46 +0530

    timetable bug fixed, where form was visible to non admin

--- client/src/screens/TimeTable.js
+++ client/src/screens/TimeTable.js
@@ -133,85 +133,87 @@ const TimeTable = () => {
         )}
       </div>
       <div className="page-layout">
-        <div className="timetable-form">
-          <div className="radio-inputs">
-            <label className="radio">
-              <input
-                type="radio"
-                name="department"
-                value="CSE"
-                checked={timetableStream === "CSE"}
-                onChange={handleStreamChange}
-              />
-              <span className="name">CSE</span>
-            </label>
-            <label className="radio">
-              <input
-                type="radio"
-                name="department"
-                value="ECE"
-                checked={timetableStream === "ECE"}
-                onChange={handleStreamChange}
-              />
-              <span className="name">ECE</span>
-            </label>
-            <label className="radio">
-              <input
-                type="radio"
-                name="department"
-                value="MCA"
-                checked={timetableStream === "MCA"}
-                onChange={handleStreamChange}
-              />
-              <span className="name">MCA</span>
-            </label>
-          </div>
+        {isAdmin &&
+          <div className="timetable-form">
+            <div className="radio-inputs">
+              <label className="radio">
+                <input
+                  type="radio"
+                  name="department"
+                  value="CSE"
+                  checked={timetableStream === "CSE"}
+                  onChange={handleStreamChange}
+                />
+                <span className="name">CSE</span>
+              </label>
+              <label className="radio">
+                <input
+                  type="radio"
+                  name="department"
+                  value="ECE"
+                  checked={timetableStream === "ECE"}
+                  onChange={handleStreamChange}
+                />
+                <span className="name">ECE</span>
+              </label>
+              <label className="radio">
+                <input
+                  type="radio"
+                  name="department"
+                  value="MCA"
+                  checked={timetableStream === "MCA"}
+                  onChange={handleStreamChange}
+                />
+                <span className="name">MCA</span>
+              </label>
+            </div>
 
-          <div className="radio-inputs">
-            <label className="radio">
-              <input
-                type="radio"
-                name="year"
-                value={1}
-                checked={timetableYear === 1}
-                onChange={handleSemesterChange}
-              />
-              <span className="name">1</span>
-            </label>
-            <label className="radio">
-              <input
-                type="radio"
-                name="year"
-                value={2}
-                checked={timetableYear === 2}
-                onChange={handleSemesterChange}
-              />
-              <span className="name">2</span>
-            </label>
-            <label className="radio">
-              <input
-                type="radio"
-                name="year"
-                value={3}
-                checked={timetableYear === 3}
-                onChange={handleSemesterChange}
-                disabled={timetableStream === "MCA"}
-              />
-              <span className="name">3</span>
-            </label>
-            <label className="radio">
-              <input
-                type="radio"
-                name="year"
-                value={4}
-                checked={timetableYear === 4}
-                onChange={handleSemesterChange}
-                disabled={timetableStream === "MCA"}
-              />
-              <span className="name">4</span>
-            </label>
+            <div className="radio-inputs">
+              <label className="radio">
+                <input
+                  type="radio"
+                  name="year"
+                  value={1}
+                  checked={timetableYear === 1}
+                  onChange={handleSemesterChange}
+                />
+                <span className="name">1</span>
+              </label>
+              <label className="radio">
+                <input
+                  type="radio"
+                  name="year"
+                  value={2}
+                  checked={timetableYear === 2}
+                  onChange={handleSemesterChange}
+                />
+                <span className="name">2</span>
+              </label>
+              <label className="radio">
+                <input
+                  type="radio"
+                  name="year"
+                  value={3}
+                  checked={timetableYear === 3}
+                  onChange={handleSemesterChange}
+                  disabled={timetableStream === "MCA"}
+                />
+                <span className="name">3</span>
+              </label>
+              <label className="radio">
+                <input
+                  type="radio"
+                  name="year"
+                  value={4}
+                  checked={timetableYear === 4}
+                  onChange={handleSemesterChange}
+                  disabled={timetableStream === "MCA"}
+                />
+                <span className="name">4</span>
+              </label>
+            </div>
           </div>
-        </div>
+        }
 
         <div className="table-container">
           <table className="table">
