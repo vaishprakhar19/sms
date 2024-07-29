b2dc91c3d00a6e78f9932381054e039008b93308
Prakhar Vaish <126570561+vaishprakhar19@users.noreply.github.com>
2024-07-29 17:35:13 +0530

    svg notice

--- client/src/screens/Notice.js
+++ client/src/screens/Notice.js
@@ -66,7 +66,7 @@ const Notice = () => {
       const url = `https://biasportalback.vercel.app/api/notices${streamSegment}${semesterSegment}`;
 
       const response = await axios.post(url, { id: newId, title, body, stream: noticeStream, semester: noticeSemester });
-      setNotices([...notices, response.data]);
+      setNotices([...notices, { id: newId, title, body, stream: noticeStream, semester: noticeSemester }]);
 
       setTitle('');
       setBody('');
@@ -104,76 +104,124 @@ const Notice = () => {
         </button>
       )}
       {isAdmin && showAddNoticeForm && (
-        <form className='notice-form' onSubmit={handleAddNotice}>
-          <div className='form-column'>
-            <div className='input-container'>
-              <input
-                className='inputField'
-                type='text'
-                placeholder='Notice title'
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                required
-              />
-            </div>
-            <div className='input-container'>
-              <textarea
-                className='inputField textareaField'
-                placeholder='Notice body'
-                value={body}
-                onChange={(e) => setBody(e.target.value)}
-                required
-              ></textarea>
-            </div>
-          </div>
-          <div className='form-column'>
-            <div className='input-container'>
-              <div className='checkbox-wrapper-46'>
-                <input type='checkbox' id='cbx-46' className='inp-cbx' checked={isNoticeGeneral} onChange={handleGeneralNoticeChange} />
-                <label htmlFor='cbx-46' className='cbx'>
-                  <span>
-                    <svg viewBox='0 0 12 10' height='10px' width='12px'>
-                      <polyline points='1.5 6 4.5 9 10.5 1'></polyline>
-                    </svg>
-                  </span>
-                  <span>General Notice</span>
-                </label>
-              </div>
-            </div>
-            <div className='input-container'>
-              <select
-                value={noticeStream || ''}
-                onChange={(e) => setNoticeStream(e.target.value)}
-                disabled={isNoticeGeneral}
-              >
-                <option value=''>Select stream</option>
-                <option value='CSE'>CSE</option>
-                <option value='IT'>IT</option>
-                <option value='ECE'>ECE</option>
-              </select>
-            </div>
-            <div className='input-container'>
-              <select
-                value={noticeSemester || ''}
-                onChange={(e) => setNoticeSemester(e.target.value)}
-                disabled={isNoticeGeneral}
-              >
-                <option value=''>Select semester</option>
-                <option value='1'>1</option>
-                <option value='2'>2</option>
-                <option value='3'>3</option>
-                <option value='4'>4</option>
-                <option value='5'>5</option>
-                <option value='6'>6</option>
-                <option value='7'>7</option>
-                <option value='8'>8</option>
-              </select>
-            </div>
-          </div>
-          <button type='submit' className='btn'>
-            Post
-          </button>
-        </form>
+         <form className="notice-form" onSubmit={handleAddNotice}>
+         <div className="form-column">
+           <div className="input-container">
+             <input
+               className="inputField"
+               type="text"
+               placeholder="Notice title"
+               value={title}
+               onChange={(e) => setTitle(e.target.value)}
+               required
+             />
+           </div>
+           <div className="input-container">
+             <textarea
+               className="inputField textareaField"
+               placeholder="Notice body"
+               value={body}
+               onChange={(e) => setBody(e.target.value)}
+               required
+             ></textarea>
+           </div>
+         </div>
+         <div className="form-column">
+           <div className="input-container">
+             <div class="checkbox-wrapper-46">
+               <input type="checkbox" id="cbx-46" class="inp-cbx"
+                 checked={isNoticeGeneral}
+                 onChange={handleGeneralNoticeChange} />
+               <label for="cbx-46" class="cbx"
+               ><span>
+                   <svg viewBox="0 0 12 10" height="10px" width="12px">
+                     <polyline points="1.5 6 4.5 9 10.5 1"></polyline></svg>
+                 </span>
+                 <span>ALL</span>
+               </label>
+             </div>
+           </div>
+           {!isNoticeGeneral && (
+             <>
+               <div className="radio-inputs notice-radio">
+                 <label className='radio'>
+                   <input
+                     type="radio"
+                     name="stream"
+                     value="CSE"
+                     checked={noticeStream === "CSE"}
+                     onChange={(e) => {
+                       setNoticeStream(e.target.value);
+                       setNoticeSemester(''); // Reset NoticeSemester value when changing stream
+                     }}
+                   />
+                   <span className="name">CSE</span>
+                 </label>
+
+
+                 <label className='radio'>
+                   <input
+                     type="radio"
+                     name="stream"
+                     value="ECE"
+                     checked={noticeStream === "ECE"}
+                     onChange={(e) => {
+                       setNoticeStream(e.target.value);
+                       setNoticeSemester(''); // Reset NoticeSemester value when changing stream
+                     }}
+                   />
+                   <span className="name">ECE</span>
+                 </label>
+                 <label className='radio'>
+                   <input
+                     type="radio"
+                     name="stream"
+                     value="MCA"
+                     checked={noticeStream === "MCA"}
+                     onChange={(e) => {
+                       setNoticeStream(e.target.value);
+                       setNoticeSemester(''); // Reset NoticeSemester value when changing stream
+                     }}
+                   />
+                   <span className="name">MCA</span>
+                 </label>
+               </div>
+               <div className="input-container">
+                 <select
+                   className="inputField"
+                   value={noticeSemester || ''} // Ensure empty string if null
+                   onChange={(e) => setNoticeSemester(e.target.value)}
+                   required={!!noticeStream} // Only required if a stream is selected
+                 >
+                   <option value="">Select Semester</option>
+                   {noticeStream === "MCA" ? (
+                     <>
+                       <option value="1">1</option>
+                       <option value="2">2</option>
+                       <option value="3">3</option>
+                       <option value="4">4</option>
+                     </>
+                   ) : (
+                     <>
+                       <option value="1">1</option>
+                       <option value="2">2</option>
+                       <option value="3">3</option>
+                       <option value="4">4</option>
+                       <option value="5">5</option>
+                       <option value="6">6</option>
+                       <option value="7">7</option>
+                       <option value="8">8</option>
+                     </>
+                   )}
+                 </select>
+               </div>
+             </>
+           )}
+         </div>
+         <div className="input-container">
+           <button id="button" type="submit">Add Notice</button>
+         </div>
+       </form>
       )}
       <div className='notices'>
       {notices.map(notice => (
