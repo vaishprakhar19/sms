0b63a9719c1352d32afc1129bf1bca1e6271ec83
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2024-08-24 20:34:46 +0530

    c

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,40 +1,40 @@
-/* eslint-disable no-restricted-globals */
-// service-worker.js
+// /* eslint-disable no-restricted-globals */
+// // service-worker.js
 
-const CACHE_NAME = "student-portal-cache-v1";
-const urlsToCache = ["/", "/index.html", "/static/js/bundle.js", "/favicon.ico"];
+// const CACHE_NAME = "student-portal-cache-v1";
+// const urlsToCache = ["/", "/index.html", "/static/js/bundle.js", "/favicon.ico"];
 
-// Install Service Worker and cache resources
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+// // Install Service Worker and cache resources
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log("Opened cache");
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
 
-// Fetch resources from cache or network
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// // Fetch resources from cache or network
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
 
-// Update the Service Worker and delete old caches
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    )
-  );
-});
+// // Update the Service Worker and delete old caches
+// self.addEventListener("activate", (event) => {
+//   const cacheWhitelist = [CACHE_NAME];
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) =>
+//       Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (!cacheWhitelist.includes(cacheName)) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       )
+//     )
+//   );
+// });
