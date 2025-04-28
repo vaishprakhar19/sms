5ca6aa6b5535ccbf1a534a89cabeb37c86875af0
Prakhar Vaish <126570561+vaishprakhar19@users.noreply.github.com>
2025-04-28 22:39:21 +0530

    Delete everything

--- backend/src/constants.js
+++
@@ -1,7 +0,0 @@
-const result_whatsapp = `
-Hi Parent,
-This message is to inform that the result for Internal Exam {} is released.
-You can access your child's result using the link below :
-{}
-`
-module.exports = { result_whatsapp }
